b5f204865550153b325922a9e4e1c814b12de65d
Angel Pastaz <angel.pastaz@epn.edu.ec>
2026-05-11 10:16:41 -0500

    Update images

--- src/components/Brands/index.tsx
+++ src/components/Brands/index.tsx
@@ -35,14 +35,7 @@ const SingleBrand = ({ brand }: { brand: Brand }) => {
 
   return (
     <div className="flex w-1/2 items-center justify-center px-3 py-[15px] sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/6">
-      <a
-        href={href}
-        target="_blank"
-        rel="nofollow noreferrer"
-        className="opacity-70 transition hover:opacity-100"
-      >
-        <Image src={image} alt={name} width={150} height={80} style={{ objectFit: 'contain' }} />
-      </a>
+      <Image src={image} alt={name} width={150} height={80} />
     </div>
   );
 };
