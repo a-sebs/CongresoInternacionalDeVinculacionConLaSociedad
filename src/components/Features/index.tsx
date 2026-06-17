92550b55208c03859c04fab97420a01e6aa8da41
Angel Pastaz <angel.pastaz@epn.edu.ec>
2026-06-17 05:39:20 -0500

    feat: Add bg-gray-light to Features/Ejes Temáticos section on home page

--- src/components/Features/index.tsx
+++ src/components/Features/index.tsx
@@ -5,7 +5,7 @@ import featuresData from "./featuresData";
 const Features = () => {
   return (
     <>
-      <section id="features" className="py-16 md:py-20 lg:py-28">
+      <section id="features" className="bg-gray-light py-16 md:py-20 lg:py-28">
         <div className="container">
           <SectionTitle
             title="Ejes Temáticos del Congreso"
